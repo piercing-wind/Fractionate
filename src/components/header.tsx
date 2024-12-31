eb97e376891dec698f354e485ec3aca250a60756
Sourabh Sharma <sourabhsharma5677@gmail.com>
2024-12-31 15:55:39 +0530

    New Pages added

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -37,6 +37,7 @@ const HeaderWithoutSessionProvider = ({className = ''}:{className ?: string}) =>
    const toggleMenu = () => {
       setIsOpen(!isOpen)
    }
+
    useEffect(() => {
       const handleClickOutside = (event: MouseEvent) => {
          if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
@@ -49,7 +50,6 @@ const HeaderWithoutSessionProvider = ({className = ''}:{className ?: string}) =>
          document.removeEventListener("mousedown", handleClickOutside);
       };
    }, [dropdownRef]);
-
    return (
       <header className={cn(`w-full sm:px-14 xl:px-28 p-2 px-4 sm:p-4`,className)}>
          <nav className="w-full flex items-center justify-between text-lg font-medium">
@@ -59,17 +59,16 @@ const HeaderWithoutSessionProvider = ({className = ''}:{className ?: string}) =>
                {/* <li className="hover:scale-105 transition-all duration-300" ><Link className={`text-nowrap px-5 py-2 rounded-full ${current === '/listings' ?  active : 'hover:bg-yellow hover:bg-opacity-55 '} transition-all duration-300`} href='/listings'>Listings</Link></li> */}
                <li className="hover:scale-105 transition-all duration-300" ><Link className={`text-nowrap px-5 py-2 rounded-full ${current === '/how-it-works' ?  active : 'hover:bg-yellow hover:bg-opacity-55 '} transition-all duration-300`} href='/how-it-works'>How it Works</Link></li>
                <li className="relative transition-all duration-300"
-                  
                >
-                  <span onMouseEnter={() => setDropdownOpen(true)}  className={`text-nowrap px-5 py-2 rounded-full hover:bg-yellow hover:bg-opacity-55 transition-all duration-300 cursor-pointer`}>Our Presence</span>
+                  <span onClick={()=>setDropdownOpen(true)} onMouseEnter={() => setDropdownOpen(true)}  className={`text-nowrap px-5 py-2 rounded-full hover:bg-yellow hover:bg-opacity-55 transition-all duration-300 cursor-pointer`}>Our Presence</span>
                   {dropdownOpen && (
                      <motion.ul
                         initial={{ opacity: 0, y: -10 }}
                         animate={{ opacity: 1, y: 0 }}
                         exit={{ opacity: 0, y: -10 }}
                         className="absolute left-0 mt-2 w-48 bg-white shadow-lg rounded-lg z-50 p-4"
-                        onMouseLeave={()=>setDropdownOpen(false)}
                         ref={dropdownRef}
+                        onMouseLeave={()=>setDropdownOpen(false)}
                      >
                         {urls.map(({ slug, name }) => (
                            <li key={slug} className="hover:bg-yellow hover:scale-105 hover:bg-opacity-55 rounded-full transition-all duration-300">
@@ -122,26 +121,25 @@ const HeaderWithoutSessionProvider = ({className = ''}:{className ?: string}) =>
                {/* <li><Link className={`text-nowrap px-5 py-2 rounded-full ${current === '/listings' ?  active : 'hover:bg-yellow hover:bg-opacity-55 '} transition-all duration-300`} href='/listings'>Listings</Link></li> */}
                <li><Link className={`text-nowrap px-5 py-2 rounded-full ${current === '/how-it-works' ?  active : 'hover:bg-yellow hover:bg-opacity-55 '} transition-all duration-300`} href='/how-it-works'>How it Works</Link></li>
                <li className="relative transition-all duration-300"
-                  
-                  >
-                     <span onMouseEnter={() => setDropdownOpen(true)}  className={`text-nowrap px-5 py-2 rounded-full hover:bg-yellow hover:bg-opacity-55 transition-all duration-300 cursor-pointer`}>Our Presence</span>
-                     {dropdownOpen && (
-                        <motion.ul
-                           initial={{ opacity: 0, y: -10 }}
-                           animate={{ opacity: 1, y: 0 }}
-                           exit={{ opacity: 0, y: -10 }}
-                           className="absolute left-0 mt-2 w-48 bg-white shadow-lg rounded-lg z-50 p-4"
-                           onMouseLeave={()=>setDropdownOpen(false)}
-                           ref={dropdownRef}
-                        >
-                           {urls.map(({ slug, name }) => (
-                              <li key={slug} className="hover:bg-yellow hover:scale-105 hover:bg-opacity-55 rounded-full transition-all duration-300">
-                                 <Link className="block px-4 py-2" href={slug}>{name}</Link>
-                              </li>
-                           ))}
-                        </motion.ul>
-                     )}
-                  </li>
+               >
+                  <span onClick={()=>setDropdownOpen(true)} onMouseEnter={() => setDropdownOpen(true)}  className={`text-nowrap px-5 py-2 rounded-full hover:bg-yellow hover:bg-opacity-55 transition-all duration-300 cursor-pointer`}>Our Presence</span>
+                  {dropdownOpen && (
+                     <motion.ul
+                        initial={{ opacity: 0, y: -10 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        exit={{ opacity: 0, y: -10 }}
+                        className="absolute left-0 mt-2 w-48 bg-white shadow-lg rounded-lg z-50 p-4"
+                        ref={dropdownRef}
+                        onMouseLeave={()=>setDropdownOpen(false)}
+                     >
+                        {urls.map(({ slug, name }) => (
+                           <li key={slug} className="hover:bg-yellow hover:scale-105 hover:bg-opacity-55 rounded-full transition-all duration-300">
+                              <Link className="block px-4 py-2 text-neutral-800" href={slug}>{name}</Link>
+                           </li>
+                        ))}
+                     </motion.ul>
+                  )}
+               </li>
                <li><Link className={`text-nowrap px-5 py-2 rounded-full ${current === '/contact' ?  active : 'hover:bg-yellow hover:bg-opacity-55 '} transition-all duration-300`} href='/contact'>Contact</Link></li>
                <li className="hover:scale-105 transition-all duration-300" >{status !== "authenticated" ? (<Link className={`text-nowrap px-5 h-8 py-2 rounded-full ${current === '/login' ?  active : 'hover:bg-yellow hover:bg-opacity-55 '} transition-all duration-300 flex items-center gap-2`} href='/login'> <UserCircle size={16}/> Login</Link> ): (<span className="flex flex-col items-center justify-center"><span className="text-yellow">Hello</span><span className="flex items-center gap-2"><HeartHandshake size={16} color="#ef4444" className="flex-shrink-0"/> {(session?.user.name as string)}</span></span>) }</li>
                {status !== "authenticated" ? <li className="hover:scale-105 transition-all duration-300" ><Link className={`text-nowrap px-5 py-2 rounded-full shadow-md bg-yellow text-white hover:bg-yellow hover:bg-opacity-55 transition-all duration-300`} href='/register'>Register</Link></li> :
--- src/middleware.ts
+++ src/middleware.ts
@@ -3,14 +3,17 @@ import authConfig from "@/auth.config";
 const { auth } = NextAuth(authConfig);
 import { apiAuthRoute, authRoute, DEFAULT_LOGIN_REDIRECT, publicRoute } from "@/route";
 
-
+const publicRoutePatterns = [
+   ...publicRoute,
+   /^\/location\/.*$/ // Matches any route that starts with /location/
+ ];
 export default auth((req) => {
   const {nextUrl}  = req;
   const isLoggedIn = !!req.auth;
   const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthRoute);
-  const isPublicRoute = publicRoute.some(route => 
-   nextUrl.pathname === route 
-  );
+  const isPublicRoute = publicRoutePatterns.some(route => 
+   typeof route === 'string' ? nextUrl.pathname === route : route.test(nextUrl.pathname)
+ );
   const isAuthRoute = authRoute.includes(nextUrl.pathname);
   //Do Not Change the order of the below id conditions. 
 
