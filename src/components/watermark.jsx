74902c64aed2fd6a41622aca059a6c4a7149f5f6
piercing-wind <sourabhsharma5677@gmail.com>
2025-08-26 22:03:39 +0530

    reference changed

--- src/components/watermark.jsx
+++ src/components/watermark.jsx
@@ -8,15 +8,15 @@ const Watermark = ({className = ""}) => {
          <p className='text-xs w-full' style={{display : 'ruby'}}>This Website is Made With &nbsp; <Heart size={16} className='fill-red-500'/>&nbsp; By</p>
       </div>
       <div className='flex flex-wrap justify-between items-center gap-2 sm:gap-0 w-full'>
-      <a href="https://acctovista.com/contact" target='_blank'>
+      <a href="https://byteswithbits.com/contact" target='_blank'>
          <button className='border flex items-center justify-center w-full px-2 flex-shrink-0 text-nowrap rounded-full min-w-[6rem] min-h-6 h-full text-xs border-purple-600 hover:bg-purple-200 hover:scale-105 transition-all duration-200'>
             Get Your website
          </button>
       </a>
-      <a href="https://acctovista.com" target='_blank'>
+      <a href="https://byteswithbits.com" target='_blank'>
          <img
-            src="/acctovista.png"
-            alt="Acctovista"
+            src="/byteswithbits.png"
+            alt="Bytes with Bits"
             width={80}
             height={10}
             className='hover:scale-105 transition-all duration-200'
